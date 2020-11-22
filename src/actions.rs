2132c077f9ab04125e8963ec25f5976be825c911
TLovell <trevorlovelldesign@gmail.com>
2020-11-21 20:33:52 -0700

    Compose now takes actions rather than closures.
    For reasons I don't totally understand, evaluating the action inside the composed closure rather than outside seems to have resolved the issue.
    Code now compiles.

--- src/actions.rs
+++ src/actions.rs
@@ -8,13 +8,10 @@ pub enum Action {
     PrintInt(usize),
 }
 
-fn compose(
-    f: Box<dyn Fn(BoxState) -> BoxResult>,
-    g: Box<dyn Fn(BoxState) -> BoxResult>,
-) -> Box<dyn Fn(BoxState) -> BoxResult> {
+fn compose(f: Action, g: Action) -> Box<dyn Fn(BoxState) -> BoxResult> {
     Box::new(move |s| {
-        let (s, g_out) = g(s);
-        let (s, f_out) = f(s);
+        let (s, g_out) = g.evaluate()(s);
+        let (s, f_out) = f.evaluate()(s);
         match (f_out, g_out) {
             (Some(out), _) => (s, Some(out)),
             (_, opout) => (s, opout),
@@ -35,10 +32,7 @@ impl Action {
                 s[*i] = s[*a] + s[*b];
                 (s, None)
             }),
-            Action::AddEq(val, i) => compose(
-                Action::Add(0, 7, *i).evaluate(),
-                Action::Set(*val, 7).evaluate(),
-            ),
+            Action::AddEq(val, i) => compose(Action::Add(*i, 7, *i), Action::Set(*val, 7)),
             Action::Mult(a, b, i) => Box::new(move |s| {
                 let mut s = s.clone();
                 s[*i] = s[*a] * s[*b];
