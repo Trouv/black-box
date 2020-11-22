daee3c507ef25594a1144d8b8879e749df831088
TLovell <trevorlovelldesign@gmail.com>
2020-11-21 22:15:24 -0700

    Simplified action solution.
    Introduced Val enum, which can either grab a value from the state (G) or just use the constant it stores (C) on evaluation.
    Closures are no longer being used, in favor of the action's functionality being a part of its evaluation.

--- src/actions.rs
+++ src/actions.rs
@@ -1,44 +1,45 @@
-use crate::components::{BoxOut, BoxResult, BoxState};
+use crate::components::{BoxOut, BoxState};
 
 pub enum Action {
-    Set(f32, usize),
-    Add(usize, usize, usize),
-    AddEq(f32, usize),
-    Mult(usize, usize, usize),
-    PrintInt(usize),
-}
-
-fn compose(f: Action, g: Action) -> Box<dyn Fn(BoxState) -> BoxResult> {
-    Box::new(move |s| {
-        let (s, g_out) = g.evaluate()(s);
-        let (s, f_out) = f.evaluate()(s);
-        match (f_out, g_out) {
-            (Some(out), _) => (s, Some(out)),
-            (_, opout) => (s, opout),
-        }
-    })
+    Set(Val, usize),
+    Add(Val, Val, usize),
+    AddEq(Val, usize),
+    Mult(Val, Val, usize),
+    PrintInt(Val),
 }
 
 impl Action {
-    pub fn evaluate<'a>(&'a self) -> Box<dyn Fn(BoxState) -> BoxResult + 'a> {
+    pub fn evaluate<'a>(&self, state: &mut BoxState) -> Option<BoxOut> {
         match self {
-            Action::Set(val, i) => Box::new(move |s| {
-                let mut s = s.clone();
-                s[*i] = *val;
-                (s, None)
-            }),
-            Action::Add(a, b, i) => Box::new(move |s| {
-                let mut s = s.clone();
-                s[*i] = s[*a] + s[*b];
-                (s, None)
-            }),
-            Action::AddEq(val, i) => compose(Action::Add(*i, 7, *i), Action::Set(*val, 7)),
-            Action::Mult(a, b, i) => Box::new(move |s| {
-                let mut s = s.clone();
-                s[*i] = s[*a] * s[*b];
-                (s, None)
-            }),
-            Action::PrintInt(i) => Box::new(move |s| (s, Some(BoxOut::Int(s[*i] as i32)))),
+            Action::Set(val, i) => {
+                state[*i] = val.evaluate(state);
+                None
+            }
+            Action::Add(a, b, i) => {
+                state[*i] = a.evaluate(state) + b.evaluate(state);
+                None
+            }
+            Action::AddEq(a, i) => Action::Add(*a, Val::G(*i), *i).evaluate(state),
+            Action::Mult(a, b, i) => {
+                state[*i] = a.evaluate(state) * b.evaluate(state);
+                None
+            }
+            Action::PrintInt(val) => Some(BoxOut::Int(val.evaluate(state) as i32)),
+        }
+    }
+}
+
+#[derive(Clone, Copy)]
+pub enum Val {
+    C(f32),
+    G(usize),
+}
+
+impl Val {
+    fn evaluate(&self, state: &BoxState) -> f32 {
+        match self {
+            Val::C(val) => *val,
+            Val::G(i) => state[*i],
         }
     }
 }
