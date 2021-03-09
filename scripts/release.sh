bb2c6df322bff81771508a98f01ade3dc93b6d59
TLovell <trevorlovelldesign@gmail.com>
2021-03-08 22:15:12 -0700

    Updated release automation so the bin is not so deep in the archive. Also renamed the folder name BACK TO the full name, so that the decompressed archive's name will match ALSO disabling macOs from the release-all for now until I can manage to cross compile it

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 NAME=$(grep ^name Cargo.toml | sed -r s/^[^\"]*\"\([^\"]*\)\"/\\1/)
 VERSION=$(grep ^version Cargo.toml | sed -r s/^[^\"]*\"\([^\"]*\)\"/\\1/)
-DIR="target/full-release/$VERSION/$1"
+FULL_NAME="$NAME-$VERSION-$1"
+DIR="target/full-release/$VERSION/$FULL_NAME"
 mkdir -p $DIR
 
 cargo build --release --target "$@"
@@ -13,8 +14,9 @@ cp COPYING $DIR
 
 if [[ $1 == *"windows"* ]]; then
   cp "target/$1/release/$NAME.exe" $DIR
-  zip -r "target/full-release/$VERSION/$NAME-$VERSION-$1.zip" $DIR
+  cd "$DIR/.."
+  zip -r "$FULL_NAME.zip" $FULL_NAME
 else
   cp "target/$1/release/$NAME" $DIR
-  tar -czvf "target/full-release/$VERSION/$NAME-$VERSION-$1.tar.gz" $DIR
+  tar -czvf "target/full-release/$VERSION/$FULL_NAME.tar.gz" -C "$DIR/.." $FULL_NAME
 fi
