5b76635a86bc7f32d7ad7c0fcff86c62a8d88cad
TLovell <trevorlovelldesign@gmail.com>
2021-03-14 16:24:52 -0600

    Now we can see all the buttons on level 1. Somehow, x and z seem to be flipped from what I'd expect..

--- src/black_state.rs
+++ src/black_state.rs
@@ -95,7 +95,7 @@ pub const CAM_RES_Y: f32 = 1080.;
 
 fn init_camera(world: &mut World) {
     let mut transform = Transform::default();
-    transform.set_translation_xyz(0., 1., -1.);
+    transform.set_translation_xyz(-1., 1., 0.);
     transform.face_towards(Vector3::new(0.0, 0.0, 0.0), Vector3::new(0.0, 1.0, 0.));
 
     world
@@ -106,12 +106,12 @@ fn init_camera(world: &mut World) {
 
     let light: Light = PointLight {
         intensity: 5.0,
-        color: Rgb::new(1.0, 0.9, 0.9),
+        color: Rgb::new(1.0, 0.8, 0.8),
         ..PointLight::default()
     }
     .into();
     let mut transform = Transform::default();
-    transform.set_translation_xyz(1., 1., 1.);
+    transform.set_translation_xyz(-1., 1., 1.);
 
     world.create_entity().with(light).with(transform).build();
 }
