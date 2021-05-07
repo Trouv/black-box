b3fd706fcb6dad92786882934c2c4d70a5f6a9d7
TLovell <trevorlovelldesign@gmail.com>
2021-05-06 21:28:56 -0600

    Removing camera_setup from transitions.rs

--- src/transitions.rs
+++ src/transitions.rs
@@ -1,18 +1,6 @@
 use crate::resources::ColorHandles;
 use bevy::prelude::*;
 
-pub fn camera_setup(mut commands: Commands) {
-    commands.spawn_bundle(UiCameraBundle::default());
-    commands.spawn_bundle(PerspectiveCameraBundle {
-        transform: Transform::from_xyz(0., 1.1, 0.8).looking_at(Vec3::new(0., 0., -0.2), Vec3::Y),
-        ..Default::default()
-    });
-    commands.spawn_bundle(LightBundle {
-        transform: Transform::from_xyz(-2., 2., 2.),
-        ..Default::default()
-    });
-}
-
 pub fn add_colors(mut materials: ResMut<Assets<ColorMaterial>>, mut commands: Commands) {
     commands.insert_resource(ColorHandles {
         white: materials.add(ColorMaterial::color(Color::rgb(0.9, 0.9, 0.9))),
