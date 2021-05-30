71dc4e0b8b21ff84aedefd4a1e5d4fd2bd744acf
TLovell <trevorlovelldesign@gmail.com>
2021-05-29 21:40:09 -0600

    Put box_internal::systems into its own file

--- src/box_internal/mod.rs
+++ src/box_internal/mod.rs
@@ -6,6 +6,7 @@
 //! Instead, other states may implement this module's systems and components as needed.
 pub mod actions;
 pub mod components;
+pub mod systems;
 
 use actions::BoxOut;
 use bevy::prelude::*;
@@ -45,74 +46,5 @@ impl TryFrom<&str> for BoxData {
         let input_path = Path::new("assets/levels").join(path);
         let f = std::fs::File::open(&input_path)?;
         from_reader(f)
-    }
-}
-
-pub mod systems {
-    use crate::box_internal::{
-        components::{ActionScript, BoxState, Itemized, PipeIn, PipePass, Pressable, Progression},
-        BoxCompletedEvent, OutputEvent,
-    };
-    use bevy::prelude::*;
-
-    pub fn button_output(
-        mut box_query: Query<&mut BoxState>,
-        button_query: Query<(&Pressable, &ActionScript, &Itemized), Changed<Pressable>>,
-        mut output_writer: EventWriter<OutputEvent>,
-    ) {
-        for (pressable, action_script, itemized) in button_query.iter() {
-            if pressable.just_unpressed() {
-                let mut box_ = box_query
-                    .get_mut(itemized.collector)
-                    .expect("Itemized component on button isn't pointing to a Box!");
-                for action in action_script {
-                    let out = action.evaluate(&mut box_);
-                    if let Some(o) = out {
-                        output_writer.send(OutputEvent {
-                            box_: itemized.collector,
-                            output: o.clone(),
-                        });
-                    }
-                }
-            }
-        }
-    }
-
-    pub fn progression(
-        mut prog_query: Query<(Entity, &mut Progression, &PipeIn)>,
-        mut output_reader: EventReader<OutputEvent>,
-        mut completed_writer: EventWriter<BoxCompletedEvent>,
-    ) {
-        for output in output_reader.iter() {
-            for (entity, mut progression, pipe_in) in prog_query.iter_mut() {
-                if let Some(e) = pipe_in.out_entity {
-                    if e == output.box_ {
-                        progression.update(output.output.clone());
-                    }
-                    if progression.progress() >= progression.total() {
-                        completed_writer.send(BoxCompletedEvent { box_: entity });
-                    }
-                }
-            }
-        }
-    }
-
-    pub fn pipe_pass(
-        pass_query: Query<(Entity, &PipeIn), With<PipePass>>,
-        mut output_reader: EventReader<OutputEvent>,
-        mut output_writer: EventWriter<OutputEvent>,
-    ) {
-        for output in output_reader.iter() {
-            for (entity, pipe_in) in pass_query.iter() {
-                if let Some(e) = pipe_in.out_entity {
-                    if e == output.box_ {
-                        output_writer.send(OutputEvent {
-                            box_: entity,
-                            ..output.clone()
-                        });
-                    }
-                }
-            }
-        }
     }
 }
---
+++ src/box_internal/systems.rs
@@ -0,0 +1,66 @@
+use crate::box_internal::{
+    components::{ActionScript, BoxState, Itemized, PipeIn, PipePass, Pressable, Progression},
+    BoxCompletedEvent, OutputEvent,
+};
+use bevy::prelude::*;
+
+pub fn button_output(
+    mut box_query: Query<&mut BoxState>,
+    button_query: Query<(&Pressable, &ActionScript, &Itemized), Changed<Pressable>>,
+    mut output_writer: EventWriter<OutputEvent>,
+) {
+    for (pressable, action_script, itemized) in button_query.iter() {
+        if pressable.just_unpressed() {
+            let mut box_ = box_query
+                .get_mut(itemized.collector)
+                .expect("Itemized component on button isn't pointing to a Box!");
+            for action in action_script {
+                let out = action.evaluate(&mut box_);
+                if let Some(o) = out {
+                    output_writer.send(OutputEvent {
+                        box_: itemized.collector,
+                        output: o.clone(),
+                    });
+                }
+            }
+        }
+    }
+}
+
+pub fn progression(
+    mut prog_query: Query<(Entity, &mut Progression, &PipeIn)>,
+    mut output_reader: EventReader<OutputEvent>,
+    mut completed_writer: EventWriter<BoxCompletedEvent>,
+) {
+    for output in output_reader.iter() {
+        for (entity, mut progression, pipe_in) in prog_query.iter_mut() {
+            if let Some(e) = pipe_in.out_entity {
+                if e == output.box_ {
+                    progression.update(output.output.clone());
+                }
+                if progression.progress() >= progression.total() {
+                    completed_writer.send(BoxCompletedEvent { box_: entity });
+                }
+            }
+        }
+    }
+}
+
+pub fn pipe_pass(
+    pass_query: Query<(Entity, &PipeIn), With<PipePass>>,
+    mut output_reader: EventReader<OutputEvent>,
+    mut output_writer: EventWriter<OutputEvent>,
+) {
+    for output in output_reader.iter() {
+        for (entity, pipe_in) in pass_query.iter() {
+            if let Some(e) = pipe_in.out_entity {
+                if e == output.box_ {
+                    output_writer.send(OutputEvent {
+                        box_: entity,
+                        ..output.clone()
+                    });
+                }
+            }
+        }
+    }
+}
